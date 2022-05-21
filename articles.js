e63a7289757b087f86a395ad4bbe381c63d8f8f3
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-05-21 15:18:59 -0300

    retrieve one article

--- articles.js
+++ articles.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
     app.get('/articles', function(request, response) {
         response.send(ARTICLES);
     });
+
+    app.get('/articles/:key', function(request, response) {
+        response.send(ARTICLES.filter(article => article.key === request.params.key)[0]);
+    });
 }
